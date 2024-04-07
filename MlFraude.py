defe8a770fdb2dce0a5dd5dd09fcc1f4f64b315c
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-04-07 19:45:18 -0300

    feat: add RandomizedSearchCV to my project

--- MlFraude.py
+++ MlFraude.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from sklearn import neighbors
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
@@ -13,14 +14,16 @@ from sklearn.metrics import accuracy_score
 TEST_SIZE = 0.2
 RANDOM_SEED = 42
 # read csv
-df = pd.read_csv('data/treino.csv', sep='|')
+url = "https://raw.githubusercontent.com/Fefevs09/Transaction-Fraud-ML/main/data/treino.csv"
+df = pd.read_csv(url,sep='|', encoding="utf-8" )
 print(df.info())
 
 # information about data
 df.select_dtypes(include="object").describe()
 
 # drop collums don't usefull
-df_train = df.drop(['ssn', 'cc_num', 'first', 'last', 'street', 'zip', 'lat', 'long', 'city_pop', 'acct_num', 'trans_num', 'merchant', 'merch_lat', 'merch_long', 'is_fraud'], axis=1)
+df = df.dropna(subset="is_fraud")
+df_train = df.drop(['ssn', 'cc_num', 'first', 'last', 'street', 'zip', 'unix_time', 'lat', 'long', 'acct_num', 'trans_num', 'merchant', 'merch_lat', 'merch_long', 'is_fraud'], axis=1)
 df_train.info()
 
 # function to separate data datetime
@@ -45,63 +48,129 @@ def get_user_age(data):
     data['age'] = (data['year'] - year_user.dt.year)
     data.drop(['dob'], axis=1, inplace=True)
 get_user_age(df_train)
-df_train.info()
+# df_train.info()
+
+# set street names to num
+def set_street_num(data):
+    data['street'] = data['street'].str.split().str[0]
+    data['street'] = data['street'].astype(int)
+
+# set_street_num(df_train)
 
 # transform category columns
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
-categorical_data = ['gender', 'city', 'state', 'job', 'profile', 'category']
-encoder = OneHotEncoder()
+# Supondo que 'df_train' seja o seu DataFrame original
 
-one_hot = encoder.fit_transform(df_train[categorical_data])
+# Extrair colunas categóricas
+categorical_data = ['gender', 'job', 'profile', 'category', 'city', 'state']
 
-encoded_df = pd.DataFrame(one_hot.toarray(), columns=encoder.get_feature_names_out(categorical_data))
+# Inicializar LabelEncoder e OneHotEncoder
+label_encoder = LabelEncoder()
+onehot_encoder = OneHotEncoder()
 
-df_encoded = pd.concat([df_train.drop(columns=categorical_data), encoded_df], axis=1)
+# Aplicar LabelEncoder para cada coluna categórica
+for column in categorical_data:
+    df_train[column] = label_encoder.fit_transform(df_train[column])
 
-# split data in train and test
+# # Aplicar OneHotEncoder para todas as colunas categóricas
+# encoded_features = onehot_encoder.fit_transform(df_train[categorical_data])
+
+# # Converter as features one-hot em DataFrame pandas
+# encoded_df = pd.DataFrame(encoded_features.toarray(), columns=onehot_encoder.get_feature_names_out(categorical_data))
+
+# # Concatenar o DataFrame one-hot com o DataFrame original
+# df_encoded = pd.concat([df_train.drop(columns=categorical_data), encoded_df], axis=1)
+
+# # Separar os dados em conjuntos de treino e teste
 from sklearn.model_selection import train_test_split
 y = df['is_fraud']
-X_train, X_test, y_train, y_test = train_test_split(df_encoded, y, test_size=TEST_SIZE, random_state=RANDOM_SEED)
 
-y_train.fillna(y_train.mean(), inplace=True)
-y_test.fillna(y_train.mean(), inplace=True)
+X_train, X_test, y_train, y_test = train_test_split(df_train, y, test_size=TEST_SIZE, random_state=RANDOM_SEED)
 
 # cleaning null values
 from sklearn.impute import SimpleImputer
-imputer = SimpleImputer()
-X_train = imputer.fit_transform(X_train)
-
-# training model
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
+
+imputer = SimpleImputer()
+X_train_transform = imputer.fit_transform(X_train)
+
 lab = LabelEncoder()
 y_train_transformed = lab.fit_transform(y_train)
-model = RandomForestClassifier()
-model.fit(X_train, y_train_transformed)
-# predict values
-X_test_transform = imputer.transform(X_test)
-y_test = lab.transform(y_test)
-pred = model.predict(X_test_transform)
-accuracy_score(y_test, pred)
 
-# response 
-df = pd.read_csv('data/teste.csv', sep='|')
-df_test = df.drop(['ssn', 'cc_num', 'first', 'last', 'street', 'zip', 'lat', 'long', 'city_pop', 'acct_num', 'trans_num', 'merchant', 'merch_lat', 'merch_long'], axis=1)
+X_test_transform = imputer.transform(X_test)
+y_test_transform = lab.transform(y_test)
+
+# training models
+# algorithms machine learning
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+
+# model = RandomForestClassifier()
+# model.fit(X_train, y_train_transformed)
+
+models = {
+    "KNN": KNeighborsClassifier(),
+    "Logist Regression": LogisticRegression(),
+    "Random Forest": RandomForestClassifier()
+}
+
+def fit_and_score(models, X_train, X_test, y_train, y_test):
+    models_scores = {}
+    for name, model in models.items():
+        model.fit(X_train, y_train)
+        models_scores[name] = model.score(X_test, y_test)
+    return models_scores
+
+
+score = fit_and_score(models=models, X_train=X_train_transform, X_test=X_test_transform, y_train=y_train_transformed, y_test=y_test_transform)
+print(score)
+
+# hyperparameters adjustements and cross validation
+from sklearn.model_selection import RandomizedSearchCV
+
+train_score = []
+test_score = []
+
+rf_grid = {
+    "n_estimators": np.arange(10, 1000, 50),
+    "max_depth": [None, 3,5,10],
+    "min_samples_split": np.arange(2,20,2),
+    "min_samples_leaf": np.arange(1, 20, 2)
+}
+rscv_rf = RandomizedSearchCV(RandomForestClassifier(),
+                             param_distributions=rf_grid,
+                             cv=5,
+                             n_iter=20,
+                             verbose=True)
+
+rscv_rf.fit(X_train_transform, y_train_transformed)
+
+print(rscv_rf.best_params_)
+print(rscv_rf.score(X_test_transform, y_test_transform))
+
+# response
+url_test = "https://raw.githubusercontent.com/Fefevs09/Transaction-Fraud-ML/main/data/teste.csv"
+df = pd.read_csv(url_test, sep='|', encoding="utf-8")
+df
+
+df_test = df.drop(['ssn', 'cc_num', 'first', 'last', 'street', 'zip', 'unix_time', 'lat', 'long', 'acct_num', 'trans_num', 'merchant', 'merch_lat', 'merch_long'], axis=1)
 df_test.info()
 cleaning_data_datetime(df_test)
 get_user_age(df_test)
 
-one_hot = encoder.transform(df_test[categorical_data])
-encoded_df = pd.DataFrame(one_hot.toarray(), columns=encoder.get_feature_names_out(categorical_data))
-df_encoded = pd.concat([df_test.drop(columns=categorical_data), encoded_df], axis=1)
+# set_street_num(df_test)
+for column in categorical_data:
+    df_test[column] = label_encoder.fit_transform(df_test[column])
 
-X_train = imputer.transform(df_encoded)
-y_sub = model.predict(X_train)
+#
+# y_sub = knn.predict(X_train)
+y_sub = rscv_rf.predict(df_test)
 
 # generate response dataframe
 response = pd.DataFrame()
 response['trans_num'] =  df['trans_num']
 response['is_fraud'] = y_sub
-response['is_fraud'].value_counts()
+# response['is_fraud'].value_counts()
 response.to_csv('data/submissao.csv', index=False)
